5c8f9010eae57b41b2e698da47f08454f0bfa145
Justin Ashworth <justin.ashworth@valid.com>
2020-01-06 12:55:38 -0500

    mac callback changes

--- src/WebWindow.Native/WebWindow.Mac.NavigationDelegate.mm
+++ src/WebWindow.Native/WebWindow.Mac.NavigationDelegate.mm
@@ -4,26 +4,25 @@
 
 - (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation;
 {
-    callUriChangedCallback(webView);
+    callUriChangedCallback([webView.URL.absoluteString UTF8String], uriChangedCallback);
 }
 
 - (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error;
 {
-    callUriChangedCallback(webView);
+    callUriChangedCallback([webView.URL.absoluteString UTF8String], uriChangedCallback);
 }
 
 - (void)webView:(WKWebView *)webView
 didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation;
 {
-    callUriChangedCallback(webView);
+    callUriChangedCallback([webView.URL.absoluteString UTF8String], uriChangedCallback);
 }
 
-void callUriChangedCallback(WKWebView *webView)
+void callUriChangedCallback(const char* uri, UriChangedCallback* callback)
 {
-    const char* uri = [webView.URL.absoluteString UTF8String];
     int length = strlen(uri);
     char* uriWritable = new char[ length + 1]();
     strncpy(uriWritable, uri, length);
-    uriChangedCallback(uriWritable);
+    callback(uriWritable);
 }
 @end
