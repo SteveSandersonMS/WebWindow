128771d2cd2168e1b698ff65db3f9b87fb2f1f91
jspuij <jws@birdseyeview.nl>
2020-03-17 16:42:22 +0100

    Native compilation problems.

--- src/WebWindow.Native/WebWindow.Windows.cpp
+++ src/WebWindow.Native/WebWindow.Windows.cpp
@@ -371,7 +371,7 @@ void WebWindow::GetAllMonitors(GetAllMonitorsCallback callback)
 {
 	if (callback)
 	{
-		EnumDisplayMonitors(NULL, NULL, MonitorEnum, (LPARAM)callback);
+		EnumDisplayMonitors(NULL, NULL, (MONITORENUMPROC)MonitorEnum, (LPARAM)callback);
 	}
 }
 
